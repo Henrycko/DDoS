df17f14ac42cb3a80b15e6ede6112183cd9aa7c5
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-06-09 16:59:25 +0700

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -19,6 +19,8 @@ def user_agent():
 	uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
 	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
 	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
+	uagent.append("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2961.52 Safari/537.6")
+
 	return(uagent)
 
 
